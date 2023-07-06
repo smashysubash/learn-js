d26c28ca68291853c935d695edf4eb8529ad8d95
subash <smashysubash@gmail.com>
2023-07-06 18:14:26 +0530

    new commit

---
+++ operator/logicaloperatorwithnon-boolean.js
@@ -0,0 +1,17 @@
+console.log(false||true)
+
+console.log(false||'Mosh')
+
+console.log(false||10)
+
+console.log(false||'mosh'||10)
+
+console.log(false||10||'Mosh')
+
+//falsy values
+
+console.log(Boolean(undefined))
+
+console.log(Boolean(null))
+
+console.log(Boolean(NaN))
